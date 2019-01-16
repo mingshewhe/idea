8cf983f6d4f8d9be36d2b555345c6360c4ed8884
ming_he <xiaoming_he@outlook.com>
2019-01-16 23:37:32 +0800

    IU-2018.1.5 <ming_he@DESKTOP-M1D18Q0 Update git.xml, project.default.xml, other.xml, Class.java, jdk.table.xml, Default.xml, path.macros.xml, find.xml, ui.lnf.xml, proxy.settings.xml, plugin_ui.xml, editor.xml, mavenVersion.xml, debugger.xml	Create lombok.xml, databaseSettings.xml, usageView.xml, mavenRunHelper.xml

--- fileTemplates/internal/Class.java
+++ fileTemplates/internal/Class.java
@@ -1,8 +1,8 @@
 #if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
 #parse("File Header.java")
 /**
-* @author xiaominghe
-* @date ${YEAR}-${MONTH}-${DAY}
+* @author ${USER}
+* @date ${DATE} ${TIME}
 */
 public class ${NAME} {
 }
