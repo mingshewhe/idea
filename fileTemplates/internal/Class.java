4efcd3ddc4280c81162570ae5155656d1dee3b29
xiaominghe <xiaominghe@lexinfintech.com>
2019-01-16 12:30:24 +0800

    IU-2018.2.6 <xiaominghe@XIAOMINGHE-PC1 Create markdown.xml, IntelliLang.xml, ide.general.xml, git.xml, project.default.xml, lombok-plugin.xml, other.xml, Default.xml, find.xml, ui.lnf.xml, plugin_ui.xml, colors.scheme.xml, mavenVersion.xml, Default.xml, laf.xml, Class.java, jdk.table.xml, debugger.renderers.xml, path.macros.xml, proxy.settings.xml, vcs.xml, mybatis.xml, editor.xml, debugger.xml, sonarlint.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,8 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+* @author xiaominghe
+* @date ${YEAR}-${MONTH}-${DAY}
+*/
+public class ${NAME} {
+}
